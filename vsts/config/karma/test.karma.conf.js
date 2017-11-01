73c143016345f9e61116fb95cf230bbbadd7382a
Bobby Earl <bobby.earl@blackbaud.com>
2017-11-01 14:10:35 -0400

    Adding real browserstack settings + deps

--- vsts/config/karma/test.karma.conf.js
+++ vsts/config/karma/test.karma.conf.js
@@ -1,14 +1,42 @@
 /*jshint node: true*/
 'use strict';
 
+// Reading credentials from command line
+const minimist = require('minimist');
+const args = minimist(process.argv.slice(2));
+
 /**
  * VSTS platform overrides.
  * @name getConfig
  * @param {Object} config
  */
 function getConfig(config) {
+
+  // Apply defaults
+  require('@blackbaud/skyux-builder/config/karma/shared.karma.conf')(config);
+
+  let customLaunchers = {
+    bs_windows_chrome_latest: {
+      base: 'BrowserStack',
+      browser: 'chrome',
+      os: 'Windows',
+      os_version: '8.1'
+    }
+  };
+
   config.set({
-      custom: true
+    browsers: Object.keys(customLaunchers),
+    customLaunchers: customLaunchers,
+    browserDisconnectTimeout: 3e5,
+    browserDisconnectTolerance: 3,
+    browserNoActivityTimeout: 3e5,
+    captureTimeout: 3e5,
+    browserStack: {
+      port: 9876,
+      pollingTimeout: 10000,
+      username: args.bsUser,
+      accessKey: args.bsKey
+    }
   });
 }
 
