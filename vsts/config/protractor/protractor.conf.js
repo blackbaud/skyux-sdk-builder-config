b3dead4df3594578a24ce9c80d280eef3bd2c599
Bobby Earl <bobby.earl@blackbaud.com>
2017-12-06 13:12:38 -0500

    Updating e2e config

--- vsts/config/protractor/protractor.conf.js
+++ vsts/config/protractor/protractor.conf.js
@@ -3,56 +3,67 @@
 
 const BrowserstackLocal = require('browserstack-local');
 const minimist = require('minimist');
-
-const commonConfig = require('@blackbaud/skyux-builder/config/protractor/protractor.conf');
+const common = require('@blackbaud/skyux-builder/config/protractor/protractor.conf');
 const merge = require('@blackbaud/skyux-builder/utils/merge');
 
 // Needed since we bypass Protractor cli
 const args = minimist(process.argv.slice(2));
 
-let bsLocal;
+// This is what ties the tests to the local tunnel that's created
+const id = 'skyux-spa-' + (new Date()).getTime();
 
-const config = {
-
+// Protractor natively accepts browserstackUser/browserstackKey, but that forces us
+// to use their baked-in seleniumAddress, which is different than what their docs say to use.
+const config = merge(common.config, {
+  directConnect: false,
   seleniumAddress: 'http://hub-cloud.browserstack.com/wd/hub',
-
   capabilities: {
     'browserstack.user': args.bsUser,
-    'browserstack.key': args.bsKey,
+    'browserstack.key':  args.bsKey,
     'browserstack.local': true,
-    browserName: 'chrome'
+    'browserstack.localIdentifier': id,
+  },
+  jasmineNodeOpts: {
+    showColors: false
   },
 
-  // Used to open the Browserstack connection
   beforeLaunch: () => {
+    require('ts-node').register({ ignore: false });
+    return new Promise((resolve, reject) => {
+      const bsConfig = {
+        key: args.bsKey,
+        onlyAutomate: true,
+        forceLocal: true,
+        force: true,
+        localIdentifier: id,
+        verbose: true
+      };
 
-    const bsConfig = {
-      key: args.bsKey
-    }
-
-    bsLocal = new BrowserstackLocal.Local();
-    bsLocal.start(bsConfig, (err) => {
-      if (err) {
-        return reject(err);
-      }
-
-      console.log('Connected to Browserstack.  Beginning e2e tests.')
-      resolve();
+      console.log('Attempting to connect to Browserstack.');
+      exports.bsLocal = new BrowserstackLocal.Local();
+      exports.bsLocal.start(bsConfig, (err) => {
+        if (err) {
+          console.error('Error connecting to Browserstack.');
+          reject(err);
+        } else {
+          console.log('Connected to Browserstack.  Beginning e2e tests.')
+          resolve();
+        }
+      });
     });
   },
 
-  // Used to close the Browserstack connection
   afterLaunch: () => {
     return new Promise((resolve, reject) => {
-      if (bsLocal) {
+      if (exports.bsLocal) {
         console.log('Closing Browserstack connection.');
-        return bsLocal.stop(resolve);
+        exports.bsLocal.stop(resolve);
+      } else {
+        console.log('Not connected to Browserstack.  Nothing to close.');
+        resolve();
       }
+    })
+});
 
-      console.log('Not connected to Browserstack.  Nothing to close.');
-      resolve();
-    });
-  }
-}
+exports.config = config;
 
-exports.config = merge(commonConfig.config, config);
