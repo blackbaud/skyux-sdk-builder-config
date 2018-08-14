8bde706b4fc0ecd6ce7ff9477fb28af44074511f
Bobby Earl <bobby.earl@blackbaud.com>
2018-08-14 14:46:27 -0400

    Revert "Created shared config for Travis-CI builds (#18)" (#20)
    This reverts commit 0d6c0c59a80e34a20288bd580ac0242969916ff3.

--- README.md
+++ README.md
@@ -1,22 +1,2 @@
-# SKY UX Builder Config
-
+# skyux-builder-config
 Configuration files for SKY UX Builder when running on different platforms.
-
-## Environment Configuration
-
-**Travis-CI (environment variables)**
-
-- `BROWSER_STACK_USERNAME`
-- `BROWSER_STACK_ACCESS_KEY`
-- `BROWSER_STACK_BUILD_ID`
-- `BROWSER_STACK_PROJECT`
-
-**VSTS (command-line arguments)**
-- `--buildNumber "BUILD_NUMBER_HERE"`
-- `--buildDefinitionName "DEF_NAME_HERE"`
-- `--bsUser ********`
-- `--bsKey ********`
-
-**Visual Tests**
-- `SKY_VISUAL_FAILURES_ACCESS_TOKEN` An access token created by [blackbaud/skyux-visualtest-results](https://github.com/blackbaud/skyux-visualtest-results) repo owners. This token is provided to each SPA on-request.
-- `SKY_VISUAL_BASELINES_ACCESS_TOKEN` An access token created by the SPA repo owners.
--- vsts/config/karma/test.karma.conf.js
+++ vsts/config/karma/test.karma.conf.js
@@ -1,11 +1,34 @@
 /*jshint node: true*/
 'use strict';
 
+const path = require('path');
 const minimist = require('minimist');
-const shared = require('../../../shared/karma/shared.karma.conf');
+const shared = require('@blackbaud/skyux-builder/config/karma/test.karma.conf');
+const logger = require('@blackbaud/skyux-logger');
+const browserUtils = require('../utils/browsers');
 
 // Needed since we bypass Karma cli
 const args = minimist(process.argv.slice(2));
+
+/**
+ * Gets any custom defined browsers and converts them to launchers.
+ * @param {*} config
+ */
+function getLaunchers(config) {
+  const browsers = browserUtils.getBrowsers(config, 'unit', {
+    base: 'BrowserStack',
+    name: 'skyux test',
+    build: args.buildNumber,
+    project: args.buildDefinitionName
+  });
+
+  // Karma needs object with key/value pairs.
+  if (browsers && browsers.length) {
+    const launchers = {};
+    browsers.forEach(browser => launchers[browser.key] = browser);
+    return launchers;
+  }
+}
 
 /**
  * VSTS platform overrides.
@@ -13,12 +36,66 @@ const args = minimist(process.argv.slice(2));
  * @param {Object} config
  */
 function getConfig(config) {
-  shared(config, {
-    BROWSER_STACK_USERNAME: args.bsUser,
-    BROWSER_STACK_ACCESS_KEY: args.bsKey,
-    BROWSER_STACK_BUILD_ID: args.buildNumber,
-    BROWSER_STACK_PROJECT: args.buildDefinitionName
+
+  // Apply defaults, needed first so we can read skyPagesConfig
+  shared(config);
+
+  // For backwards compatability, Karma overwrites arrays
+  config.reporters.push('junit');
+  config.coverageReporter.reporters.push({
+    type: 'cobertura'
   });
+
+  // These are general VSTS overrides, regardless of Browserstack
+  const overrides = {
+    browserDisconnectTimeout: 6e5,
+    browserDisconnectTolerance: 3,
+    browserNoActivityTimeout: 6e5,
+    captureTimeout: 6e5,
+    junitReporter: {
+      outputDir: path.join(process.cwd(), 'test-results')
+    },
+
+    // VSTS doesn't render default symbols well.
+    mochaReporter: {
+      symbols: {
+        success: '+',
+        info: '#',
+        warning: '!',
+        error: 'x'
+      }
+    }
+  };
+
+  // Only override certain properties if there are customLaunchers
+  const launchers = getLaunchers(config);
+  if (launchers) {
+    overrides.customLaunchers = launchers;
+    overrides.browsers = Object.keys(launchers);
+    overrides.browserStack = {
+      port: 9876,
+      pollingTimeout: 10000,
+      timeout: 600,
+      username: args.bsUser,
+      accessKey: args.bsKey,
+      enableLoggingForApi: true
+    };
+
+    // Custom plugin used to read the Browserstack session
+    // Pushing to the original config since arrays aren't merged.
+    config.reporters.push('blackbaud-browserstack');
+    config.plugins.push({
+      'reporter:blackbaud-browserstack': [
+        'type',
+        function (/* BrowserStack:sessionMapping */ sessions) {
+          this.onBrowserComplete = (browser) => browserUtils.logSession(sessions[browser.id]);
+        }
+      ]
+    });
+  }
+
+  config.set(overrides);
+  logger.verbose(config);
 }
 
 module.exports = getConfig;
